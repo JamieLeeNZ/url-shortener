368a9a174ab5a405a9354ede5efad5a82daf2771
Jamie Lee <126450738+eejl773@users.noreply.github.com>
2025-06-26 22:50:25 +1200

    added logging for cache hit or miss

--- store/cached.go
+++ store/cached.go
@@ -1,5 +1,9 @@
 package store
 
+import (
+	"log"
+)
+
 type CachedStore struct {
 	cache URLStore
 	db    URLStore
@@ -20,10 +24,14 @@ func (s *CachedStore) Set(key, originalURL string) error {
 
 func (s *CachedStore) GetOriginalFromKey(key string) (string, bool) {
 	if original, found := s.cache.GetOriginalFromKey(key); found {
+		log.Printf("[cache] hit for key: %s", key)
 		return original, true
 	}
+	log.Printf("[cache] miss for key: %s", key)
+
 	original, found := s.db.GetOriginalFromKey(key)
 	if found {
+		log.Printf("[db] fetched and caching key: %s", key)
 		s.cache.Set(key, original)
 	}
 	return original, found
@@ -31,10 +39,14 @@ func (s *CachedStore) GetOriginalFromKey(key string) (string, bool) {
 
 func (s *CachedStore) GetKeyFromOriginal(original string) (string, bool) {
 	if key, found := s.cache.GetKeyFromOriginal(original); found {
+		log.Printf("[cache] hit for original URL: %s", original)
 		return key, true
 	}
+	log.Printf("[cache] miss for original URL: %s", original)
+
 	key, found := s.db.GetKeyFromOriginal(original)
 	if found {
+		log.Printf("[db] fetched and caching original URL: %s", original)
 		s.cache.Set(key, original)
 	}
 	return key, found
